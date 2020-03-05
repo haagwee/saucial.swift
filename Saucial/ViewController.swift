922937ec0c92ec56f60a55a580e16174f04a1f31
Dunya Kirkali <dunyakirkali@gmail.com>
2020-03-05 07:57:00 +0100

    Initial Commit

---
+++ Saucial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saucial
+//
+//  Created by Dunya Kirkali on 05/03/2020.
+//  Copyright © 2020 Dunya Kirkali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
